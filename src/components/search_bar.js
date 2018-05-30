85ce5618ee97d018a01ff8e2d57026f470b6a34d
Ogi G Permana <hunterdumay@gmail.com>
2018-05-30 13:58:56 +0700

    Pengenalan State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,20 @@
 import React, {Component}  from 'react';
 
 class SearchBar extends Component{
+	constructor(props){
+			super(props);
+
+			this.state = { term: '' };
+	}
+
 	render(){
-		return <input onChange={event => console.log(event.target.value)} />;
+		return (
+		<div>
+			<input onChange={event => this.setState({ term: event.target.value })} />
+			// <br />
+			// Nilai dari inputan: {this.state.term} <- menampilkan inputan
+		</div>
+		);
 	}
 }
 
