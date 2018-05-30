bd2492156d38c374d564bed1dd91409e974004d4
Ogi G Permana <hunterdumay@gmail.com>
2018-05-30 12:52:01 +0700

    Export statement dan Penggunaan Class-Based Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBTGBU5RKjbMggC1nPspGpCqz4NE-jQMbI';
+
 // Create new component. This component should produce
 // some HTML
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // Take this component's generated HTML and put it
